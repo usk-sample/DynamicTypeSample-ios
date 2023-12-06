ced9569b329ac5eed1da31ef508374be03ef0b8c
Yusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2023-12-07 08:41:56 +0900

    Initial Commit

---
+++ DynamicTypeSample/DynamicTypeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTypeSampleApp.swift
+//  DynamicTypeSample
+//
+//  Created by Yusuke Hasegawa on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTypeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
