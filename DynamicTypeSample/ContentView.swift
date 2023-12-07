ed3c1c0f67b68d16fb8407478999f10ec4f7fa6e
Yusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2023-12-07 13:30:25 +0900

    feat: get dynamic type information

--- DynamicTypeSample/ContentView.swift
+++ DynamicTypeSample/ContentView.swift
@@ -8,17 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var appDelegate: AppDelegate
+    @State var dynamicTypeSize: DynamicTypeSize = .xSmall
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    let sizes: [DynamicTypeSize] = DynamicTypeSize.allCases
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("current : \(dynamicTypeSize.description)")
+            Text("Aa あ")
+                .dynamicTypeSize(dynamicTypeSize)
+                .frame(height: 100)
+            LazyVGrid(columns: columns) {
+                ForEach(sizes.indices, id: \.self) { index in
+                    Button(sizes[index].description) {
+                        self.dynamicTypeSize = sizes[index]
+                    }
+                }
+            }
         }
         .padding()
+        .onReceive(appDelegate.$dynamicTypeSize) { value in
+            dynamicTypeSize = value
+        }
+        .onAppear {
+            dynamicTypeSize = appDelegate.dynamicTypeSize
+        }
+    }
+}
+
+extension DynamicTypeSize: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .xSmall:
+            return "xSmall"
+        case .small:
+            return "small"
+        case .medium:
+            return "medium"
+        case .large:
+            return "large"
+        case .xLarge:
+            return "xLarge"
+        case .xxLarge:
+            return "xxLarge"
+        case .xxxLarge:
+            return "xxxLarge"
+        case .accessibility1:
+            return "accessibility1"
+        case .accessibility2:
+            return "accessibility2"
+        case .accessibility3:
+            return "accessibility3"
+        case .accessibility4:
+            return "accessibility4"
+        case .accessibility5:
+            return "accessibility5"
+        @unknown default:
+            fatalError()
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .environmentObject(AppDelegate())
 }
--- DynamicTypeSample/DynamicTypeSampleApp.swift
+++ DynamicTypeSample/DynamicTypeSampleApp.swift
@@ -7,11 +7,40 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+    
+    @Published var dynamicTypeSize: DynamicTypeSize = .xSmall
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        debugPrint("dynamic type : \(application.preferredContentSizeCategory.rawValue)")
+
+        NotificationCenter.default.addObserver(self, selector: #selector(contentSizeUpdate), name: UIContentSizeCategory.didChangeNotification, object: nil)
+        
+        contentSizeUpdate()
+        
+        return true
+    }
+    
+    @objc func contentSizeUpdate() {
+        debugPrint("contentSizeUpdate")
+        
+        dynamicTypeSize = .init(UIApplication.shared.preferredContentSizeCategory) ?? .xSmall
+        
+    }
+    
+}
+
 @main
 struct DynamicTypeSampleApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appDelegate)
         }
     }
+    
 }
